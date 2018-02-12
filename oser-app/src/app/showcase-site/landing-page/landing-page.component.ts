a1a6481389fccfa216e9657f94614c5ecc9a3c0c
florimondmanca <florimond.manca@gmail.com>
2018-02-12 01:18:59 +0100

    add landing page and footer

---
+++ oser-app/src/app/showcase-site/landing-page/landing-page.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { News } from './news';
+import { NEWS } from './mock-news';
+
+class Action {
+  title: string;
+  src: string;
+}
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent implements OnInit {
+
+  actions: Action[] = [
+    {
+      title: 'Apprendre, comprendre',
+      src: 'assets/img/action-apprendre.jpg'
+    },
+    {
+      title: "Découvrir, s'intéresser",
+      src: 'assets/img/action-decouvrir.jpg'
+    },
+    {
+      title: "Vivre l'extra-ordinaire",
+      src: 'assets/img/action-vivre.jpg'
+    }
+  ];
+
+  news: News[];
+
+  constructor() {
+    this.getNews();
+  }
+
+  getNews(): void {
+    this.news = NEWS;
+  }
+
+  ngOnInit() {
+  }
+
+}
--- oser-app/src/app/showcase-site/showcase-site.module.ts
+++ oser-app/src/app/showcase-site/showcase-site.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ShowcaseSiteComponent } from './showcase-site.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NewsCardComponent } from './landing-page/news-card/news-card.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavbarComponent, ShowcaseSiteComponent],
+  declarations: [NavbarComponent, ShowcaseSiteComponent, LandingPageComponent, NewsCardComponent, FooterComponent],
   exports: [ShowcaseSiteComponent]
 })
 export class ShowcaseSiteModule { }
