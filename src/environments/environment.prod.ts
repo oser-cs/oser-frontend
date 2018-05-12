1489e7f3f8dc1421ecf848af02326f3bee0c1b9f
florimondmanca <florimond.manca@gmail.com>
2018-05-12 16:33:28 +0100

    fix showcase site api prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
+import { environment as devEnv } from './environment';
+
 export const environment = {
   production: true,
-  showcaseApiUrl: 'https://oser-site-vitrine.herokuapp.com/api',
+  showcaseApiUrl: devEnv.showcaseApiUrl,
   apiUrl: 'https://oser-backend.herokuapp.com/api/',
 };
