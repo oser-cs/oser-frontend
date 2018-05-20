521443a758afc82e65b6dec8ddc3a016b9cf770d
florimondmanca <florimond.manca@gmail.com>
2018-05-20 23:02:23 +0100

    fix showcase api url missing trailing slash

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  showcaseApiUrl: 'https://oser-site-vitrine.herokuapp.com/api',
+  showcaseApiUrl: 'https://oser-site-vitrine.herokuapp.com/api/',
   apiUrl: 'https://oser-backend.herokuapp.com/api/',
 };
