4e974f67b7849e743800cb5371c0c717a2463f33
florimondmanca <florimond.manca@gmail.com>
2018-05-27 15:05:23 +0100

    fix nonexistant import

--- src/app/showcase-site/actions-page/actions-page.component.ts
+++ src/app/showcase-site/actions-page/actions-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {
-  Testimony, TestimonyService,
+  Testimony,
   Action, ActionService
 } from 'app/showcase-site/shared';
 
