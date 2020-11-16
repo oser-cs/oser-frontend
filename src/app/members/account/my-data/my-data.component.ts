32a360ff046bb7ab9032023206e8e105372765c9
Arthur Guédon <60623551+arthurgdn@users.noreply.github.com>
2020-11-16 19:09:54 +0100

    Validated signup file display (#54)
    * adds support for personnal data
    
    * modifie le form pour inclure la nationalité
    
    * supprime le gg form
    
    * supprime le champ section et ajoute la liste des etablissements
    
    * adapts for user
    
    * adds validation and connects to students api for personnal data
    
    * adds classType field and rgpd info
    
    * corrects scholarships
    
    * removes signup file tab
    
    * minor presentation changes
    
    * deletes random console.log
    
    * changes upload component declarations
    
    * changes upload component declarations
    
    * changes upload component declarations
    
    * changes upload component declarations
    
    * changes upload component declarations
    
    * changes upload component declarations
    
    * changes upload component declarations
    
    * filters if the whole list is sent back
    
    * adds message after personnal data modifications
    
    * adds untested frontend support for showing validated status of signup file
    
    * adds phone number validation for personnal data
    
    * checks for year field in registration to display correct status

--- src/app/members/account/my-data/my-data.component.ts
+++ src/app/members/account/my-data/my-data.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute,Router} from '@angular/router'
 import {PersonalData} from '../core'
+import * as moment from 'moment'
 import {User} from 'app/core'
 
 @Component({
@@ -11,7 +12,9 @@ import {User} from 'app/core'
 export class MyDataComponent implements OnInit {
   
   personalData: PersonalData;
-  
+  validatedAccount : String;
+  dateNow: Date;
+  currentYear: String;
   
   public grade = {
     "troisieme": "Troisième",
@@ -19,6 +22,17 @@ export class MyDataComponent implements OnInit {
     "premiere":"Première",
     "terminale":"Terminale"
 
+  }
+  
+  //Styles validated account label depending on the status of the account
+  public styleValidatedAccount = ()=> {
+    if(this.validatedAccount==="Données personnelles non remplies"){
+      return 'not-sent-label'
+    }else if (this.validatedAccount==="En cours de validation"){
+      return 'in-progress-label'
+    }else if (this.validatedAccount==="Validé"){
+      return 'validated-label'
+    }
   }
   public scholarship = {
     "echelon0": "Oui, échelon 0",
@@ -45,7 +59,21 @@ export class MyDataComponent implements OnInit {
   
   ngOnInit() {
     
-    this.personalData = this.route.snapshot.data['personalData'];
+    this.personalData = this.route.snapshot.data['personalData']
+    this.dateNow = new Date()
+    if(moment(this.dateNow).month()>=9){
+      this.currentYear = moment(this.dateNow).year() + "/"+ String(moment(this.dateNow).year()+1)
+    }else{
+      this.currentYear = String(moment(this.dateNow).year() -1) + "/"+moment(this.dateNow).year()
+    }
+    
+    if(this.personalData.registration.validated){
+      this.validatedAccount= "Validé"
+    }else if(this.personalData.year===this.currentYear){
+      this.validatedAccount="En cours de validation"
+    }else{
+      this.validatedAccount="Données personnelles non remplies"
+    }
     
   }
 
