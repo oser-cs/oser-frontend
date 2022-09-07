52df82c68f20a03bc1cb70db2e2d1d93bd8ca9ea
Aidli Melissa <melissa.aidli@student-cs.fr>
2022-09-07 22:39:57 +0200

    Updated routes

--- src/app/signup/signup-routing.module.ts
+++ src/app/signup/signup-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentCharterComponent } from './student-charter/student-charter.component';
 import { StudentSignupComponent } from './student-signup/student-signup.component';
+import { ValidEmailComponent } from './valid-email/valid-email.component';
+import { WaitingEmailVerificationComponent } from './waiting-email-verification/waiting-email-verification.component';
+import { ParentsCharterComponent } from './parents-charter/parents-charter.component';
 
 
 const routes: Routes = [
@@ -8,6 +12,23 @@ const routes: Routes = [
     path: '',
     component: StudentSignupComponent
   },
+  {
+    path:'waiting',
+    component : WaitingEmailVerificationComponent
+  },
+  {
+    path:'valid-email',
+    component : ValidEmailComponent
+  },
+  {
+    path:'student-charter',
+    component : StudentCharterComponent
+  },
+  {
+    path:'parents-charter',
+    component : ParentsCharterComponent
+  }
+
 ];
 
 @NgModule({
