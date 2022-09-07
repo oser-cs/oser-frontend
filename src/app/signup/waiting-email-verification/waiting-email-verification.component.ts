a96c0c6d0ef504673a7eb7e9d6f64eb6f6845709
Aidli Melissa <melissa.aidli@student-cs.fr>
2022-09-07 22:42:03 +0200

    Waiting email verification

---
+++ src/app/signup/waiting-email-verification/waiting-email-verification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-waiting-email-verification',
+  templateUrl: './waiting-email-verification.component.html',
+  styleUrls: ['./waiting-email-verification.component.scss']
+})
+export class WaitingEmailVerificationComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
